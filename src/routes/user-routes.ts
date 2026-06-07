4a51e82ee02599aef160f3529bedef749e668718
Moises de Jesus <moises.de.jesus.profissional@gmail.com>
2026-06-07 02:08:23 -0300

    feat: add async to routes

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -4,11 +4,11 @@ import UserController from '../controllers/user-controller.js';
 const userController: UserController = new UserController()
 const userRouter: Router = express.Router()
 
-userRouter.get("/clientes/name/:name",(req: Request,res: Response) => userController.getAllUserByName(req,res))
-userRouter.get("/clientes/email/:email",(req: Request,res: Response) => userController.getAllUserByEmail(req,res))
-userRouter.get("/clientes/id/:id",(req: Request,res: Response) => userController.getAllUserById(req,res))
-userRouter.post("/clientes/add",(req: Request,res: Response) => userController.createUser(req,res))
-userRouter.put("/clientes/change/:id", (req: Request, res: Response) => userController.updateUserById(req, res))
-userRouter.delete("/clientes/delete/:id",(req: Request,res: Response) => userController.deleteUserById(req,res))
+userRouter.get("/clientes/name/:name",async (req: Request,res: Response) => await userController.getAllUserByName(req,res))
+userRouter.get("/clientes/email/:email",async (req: Request,res: Response) => await userController.getAllUserByEmail(req,res))
+userRouter.get("/clientes/id/:id",async (req: Request,res: Response) => await userController.getAllUserById(req,res))
+userRouter.post("/clientes/add",async (req: Request,res: Response) => await userController.createUser(req,res))
+userRouter.put("/clientes/change/:id", async (req: Request, res: Response) => await userController.updateUserById(req, res))
+userRouter.delete("/clientes/delete/:id",async (req: Request,res: Response) => await userController.deleteUserById(req,res))
 
 export default userRouter
