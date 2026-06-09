522966f11ecdeb9dc448a2c0d8fec0e3e267812d
Moises de Jesus <moises.de.jesus.profissional@gmail.com>
2026-06-09 01:54:41 -0300

    feat: add swagger documentation to routes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express, { Express } from 'express';
+import swaggerUi from 'swagger-ui-express';
 import userRouter from './routes/user-routes.js';
+import swaggerDocs from './swagger.json' with {type: "json"};
 
 const app: Express=express()
 const port: Number=Number(process.env.PORT) || 3000
 app.use(express.json())
 app.use("/",userRouter)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.listen(port, () => {console.log(`Starting Api on port ${port}`)})
